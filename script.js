fcefad8bdc4c25167ee8f4d726052030c85aac77
HadasYemini <hadassyemini@gmail.com>
2024-05-21 12:12:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//? Question1
+const fs = require('fs')
+
+fs.readFile("file.txt", (err, data) => {
+    if (err) throw 'error';
+  
+    console.log(data.toString());
+    let arr=data.toString().replace('\r\n',' ').split(' ')
+    console.log(arr.length)
+  });
+
+  //? Question2
+  const readXlsxFile = require('read-excel-file/node')
+
+  // File path.
+  readXlsxFile('./students.xlsx').then((rows) => {
+    // `rows` is an array of rows
+    // each row being an array of cells.
+    console.log(rows);
+    let sum = 0
+    rows.forEach(element => sum+=element[1])
+    console.log(sum/rows.length)
+  })
